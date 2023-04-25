5e43ff8f012f2f9dbba459873d1cfb94cddea1d6
Bosun Olisa <olatunbosun.olisa93@gmail.com>
2023-04-24 22:27:23 -0500

    Added code for dev and prod

--- sample/outputs.tf
+++ sample/outputs.tf
@@ -5,7 +5,7 @@ output "sample_string" {
     value = var.sample_string
 }
 
-## Printing the variable above
+## Printing the variable above; ${} is mandatory if we include the variable inside a string
 
 output "sample_string1" {
     value = "var.sample_string = ${var.sample_string}"
@@ -16,7 +16,7 @@ output "sample_string1" {
 ## Values from list
 # Second value in list 
 output "sample_list2" {
-    value = var.sample_list2
+    value = var.sample_list[1]
 }
 
 # Print number1 in sample_dict
@@ -25,5 +25,9 @@ output "sample_dict_num1" {
 }
 
 output "sample_dict_bool" { 
-    value = var.sample_dict[1]
+    value = var.sample_dict["boolean"]
+}
+
+output "env" {
+  value = var.env
 }
